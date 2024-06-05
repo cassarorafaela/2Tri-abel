777085d3c6d9b19cc710b62c8b0cb47c353ad65b
rafaela.cassaro <108940635+cassarorafaela@users.noreply.github.com>
2024-06-05 09:43:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultados = document.querySelector(".caixa-resultados")
 const textoResultados = document.querySelector(".texto-resultado")
-const perguntas = [
+
+const perguntas = [ 
     {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
+ enunciado: "Pergunta 1",
+ alternativas: [  "Alternativa 1", "Alternativa 2"   ]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
+  enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1","Alternativa 2"  ]
     }
 ]; 
