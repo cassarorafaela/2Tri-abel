0096199680067f936c4b50ef0f854b7934587958
rafaela.cassaro <108940635+cassarorafaela@users.noreply.github.com>
2024-06-12 11:29:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -47,5 +47,13 @@ function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
+mostraAlternativas();
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
 
 mostraPergunta();
